628aa1eeae21d3b5915fcb357d7c6d3abfec7b04
jihoonkim <j5kim@ucsd.edu>
2017-01-25 18:32:45 -0800

    fix error in sed command with /etc/hosts

--- provision/install_gnome_and_nomachine.sh
+++ provision/install_gnome_and_nomachine.sh
@@ -22,8 +22,7 @@ dpkg -i nomachine_5.1.62_1_amd64.deb
 apt-get install -y gnome-core ubuntu-gnome-desktop
 
 # disable gnome screen saver 
-gsettings set org.gnome.settings-daemon.plugins.power idle-dim false
-gsettings set org.gnome.desktop.session idle-delay 0
+
 
 # change the NX authentication to that of sshd
 mv /etc/pam.d/nx  /etc/pam.d/nx-default
@@ -33,7 +32,7 @@ cp /etc/pam.d/sshd /etc/pam.d/nx
 sed -i 's/NXPort 4000/NXPort 5091/g' /usr/NX/etc/server.cfg
 
 # add 'localhost' to the end of line starting with 127.0.0.1 in /etc/hosts
-sed -i '/^127.0.0.1/ s/$/ localhost'  /etc/hosts
+sed -i '/^127.0.0.1/ s/$/ localhost/'  /etc/hosts
 
 # restart NX server
 service nxserver start 
