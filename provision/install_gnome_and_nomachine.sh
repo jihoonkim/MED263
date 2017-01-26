22d4c6edbf9496eac43e74a02d4dc85928cbc7b0
jihoonkim <j5kim@ucsd.edu>
2017-01-25 18:37:09 -0800

    remove apt-get update

--- provision/install_gnome_and_nomachine.sh
+++ provision/install_gnome_and_nomachine.sh
@@ -7,8 +7,6 @@
 #                      NOMACHINE for remote desktop access to ubuntu VM
 # Date: 1/25/2017
 #-----------------------------------------------------------------------------
-# update Ubuntu
-apt-get update -y
 
 # close the opened port 25 for mail server 
 systemctl stop postfix
@@ -29,7 +27,7 @@ mv /etc/pam.d/nx  /etc/pam.d/nx-default
 cp /etc/pam.d/sshd /etc/pam.d/nx 
 
 # change the NX port number to 5091
-sed -i 's/NXPort 4000/NXPort 5091/g' /usr/NX/etc/server.cfg
+sed -i 's/#NXPort 4000/NXPort 5091/g' /usr/NX/etc/server.cfg
 
 # add 'localhost' to the end of line starting with 127.0.0.1 in /etc/hosts
 sed -i '/^127.0.0.1/ s/$/ localhost/'  /etc/hosts
