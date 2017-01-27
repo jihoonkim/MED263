1b419a081cbba4a6e12e20fddcfad89f149deea8
jihoonkim <j5kim@ucsd.edu>
2017-01-26 17:42:15 -0800

    add commands to disable NX server requesting authorization to the owner of the desktop before remote connecting

--- provision/install_gnome_and_nomachine.sh
+++ provision/install_gnome_and_nomachine.sh
@@ -27,7 +27,6 @@ cp /etc/pam.d/sshd /etc/pam.d/gdm-autologin
 mv /etc/pam.d/gdm-password /etc/pam.d/gdm-password-default
 cp /etc/pam.d/sshd /etc/pam.d/gdm-password
 
-
 # start GNOME Display Manager (GDM) 
 systemctl start gdm3 
 
@@ -41,6 +40,14 @@ cp /etc/pam.d/sshd /etc/pam.d/nx
 
 # change the NX port number to 5901
 sed -i 's/#NXPort 4000/NXPort 5901/g' /usr/NX/etc/server.cfg
+
+# disable NX Server requesting authorization to the owner of the virtual 
+#   desktop before connecting
+sed -i 's/#VirtualDesktopAuthorization 1/VirtualDesktopAuthorization 0/g' /usr/NX/etc/server.cfg
+
+# disable NX Server requesting authorization to the owner of the physical 
+#    desktop before connecting.
+sed -i 's/#PhysicalDesktopAuthorization 1/PhysicalDesktopAuthorization 0/g' /usr/NX/etc/server.cfg
 
 # add 'localhost' to the end of line starting with 127.0.0.1 in /etc/hosts
 sed -i '/^127.0.0.1/ s/$/ localhost/'  /etc/hosts
