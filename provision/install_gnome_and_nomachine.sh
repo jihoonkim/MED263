9a1efae531bae4f6265cbf948604a2ae00e8174e
jihoonkim <j5kim@ucsd.edu>
2017-01-26 18:28:32 -0800

    add reboot in the last line

--- provision/install_gnome_and_nomachine.sh
+++ provision/install_gnome_and_nomachine.sh
@@ -27,9 +27,6 @@ cp /etc/pam.d/sshd /etc/pam.d/gdm-autologin
 mv /etc/pam.d/gdm-password /etc/pam.d/gdm-password-default
 cp /etc/pam.d/sshd /etc/pam.d/gdm-password
 
-# start GNOME Display Manager (GDM) 
-systemctl start gdm3 
-
 # install NoMachine server
 wget http://download.nomachine.com/download/5.1/Linux/nomachine_5.1.62_1_amd64.deb
 dpkg -i nomachine_5.1.62_1_amd64.deb
@@ -52,5 +49,11 @@ sed -i 's/#PhysicalDesktopAuthorization 1/PhysicalDesktopAuthorization 0/g' /usr
 # add 'localhost' to the end of line starting with 127.0.0.1 in /etc/hosts
 sed -i '/^127.0.0.1/ s/$/ localhost/'  /etc/hosts
 
+# start GNOME Display Manager (GDM) 
+systemctl start gdm3 
+
 # restart NX server
 service nxserver start 
+
+# reboot the server
+reboot
