675e0288cead18dacee414a295b7f3a0123e761f
jihoonkim <j5kim@ucsd.edu>
2017-03-02 11:50:22 -0800

    add sourcing /etc/profile

--- provision/install_Jupyter.sh
+++ provision/install_Jupyter.sh
@@ -6,7 +6,7 @@ apt-get upgrade -y
 apt-get update -y 
 
 # install dependent packages
-apt-get install libcurl4-openssl-dev libssl-dev
+apt-get install -ylibcurl4-openssl-dev libssl-dev
 
 # download and install Anaconda
 wget https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh
@@ -14,9 +14,9 @@ bash Anaconda2-4.3.0-Linux-x86_64.sh -b -p /opt/anaconda2
 
 # add Anaconda path
 echo -e 'export PATH=/opt/anaconda2/bin:$PATH'  >> /etc/profile
-source ~/.bashrc 
+source /etc/profile
 
-# create an R script for a batch installation
+# create a script for a batch installation of R packages
 cat > install_Jupyter_Rpackages.R << EOL
 myRepo = "http://cran.cnr.berkeley.edu/"
 myWishList = c("crayon", "curl", "devtools", "digest", "evaluate", "git2r", "httr", "IRdisplay", "IRkernel", "jsonlite", "memoise", "openssl", "pbdZMQ", "repr", "uuid", "whisker", "withr")
@@ -27,5 +27,5 @@ devtools::install_github('IRkernel/IRkernel')
 IRkernel::installspec()
 EOL
 
-# run R installation script
+# run the script for a batch installation of R packages
 Rscript install_Jupyter_Rpackages.R
