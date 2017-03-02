00b156cd42d3d171a8bd4eb86305be58e5783caf
jihoonkim <j5kim@ucsd.edu>
2017-03-02 11:37:27 -0800

    add a batch script

---
+++ provision/install_Jupyter.sh
@@ -0,0 +1,31 @@
+# become a root 
+sudo su -
+
+# update and upgrade Ubuntu packages
+apt-get upgrade -y
+apt-get update -y 
+
+# install dependent packages
+apt-get install libcurl4-openssl-dev libssl-dev
+
+# download and install Anaconda
+wget https://repo.continuum.io/archive/Anaconda2-4.3.0-Linux-x86_64.sh
+bash Anaconda2-4.3.0-Linux-x86_64.sh -b -p /opt/anaconda2 
+
+# add Anaconda path
+echo -e 'export PATH=/opt/anaconda2/bin:$PATH'  >> /etc/profile
+source ~/.bashrc 
+
+# create an R script for a batch installation
+cat > install_Jupyter_Rpackages.R << EOL
+myRepo = "http://cran.cnr.berkeley.edu/"
+myWishList = c("crayon", "curl", "devtools", "digest", "evaluate", "git2r", "httr", "IRdisplay", "IRkernel", "jsonlite", "memoise", "openssl", "pbdZMQ", "repr", "uuid", "whisker", "withr")
+targetLibDir = "/usr/lib/R/library"
+options(repos = c(CRAN = myRepo))
+install.packages(myWishList, lib=targetLibDir)
+devtools::install_github('IRkernel/IRkernel')
+IRkernel::installspec()
+EOL
+
+# run R installation script
+Rscript install_Jupyter_Rpackages.R
